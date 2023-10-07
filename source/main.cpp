a9cfe45ea8a9b3121d7debac987c8fb68dd4575a
Nightkingale <63483138+Nightkingale@users.noreply.github.com>
2023-10-07 11:52:10 -0600

    Prepare for next major release

--- source/main.cpp
+++ source/main.cpp
@@ -59,7 +59,7 @@ using namespace std::literals;
 // Important plugin information.
 WUPS_PLUGIN_NAME(PLUGIN_NAME);
 WUPS_PLUGIN_DESCRIPTION("A plugin that synchronizes a Wii U's clock to the Internet.");
-WUPS_PLUGIN_VERSION("v1.1.0");
+WUPS_PLUGIN_VERSION("v2.0.0");
 WUPS_PLUGIN_AUTHOR("Nightkingale, Daniel K. O.");
 WUPS_PLUGIN_LICENSE("MIT");
 
